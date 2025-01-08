5e5e4a1705614d9fa609beb204040214b772eb95
Nicopr06 <jose.a.aparisi1@gmail.com>
2025-01-08 19:19:06 +0100

    lista

--- Python/Bucles/bucles.py
+++ Python/Bucles/bucles.py
@@ -6,22 +6,47 @@
 
 # projecto2
  
-lista = [33, 27, 99, 22, 45, 54]
+# lista = [33, 27, 99, 22, 45, 54]
+# 
+# min = 100
+# maj = 0
+# 
+# for i in lista: 
+    # if(i < min):
+        # min = i
+# 
+# print(f"El menor es {min}")
+# 
+# for i in lista: 
+    # if(i > maj):
+        # maj = i
+        # 
+# print(f"El mayor es {maj}")
+# 
+# med = sum(lista) / len(lista)
+# print(f"La media es {med}")
 
-min = 100
-maj = 0
+# Range
 
-for i in lista: 
-    if(i < min):
-        min = i
+# for i in range(10, -1, -1):
+    # print(i)
 
-print(f"El menor es {min}")
+# While
 
-for i in lista: 
-    if(i > maj):
-        maj = i
-        
-print(f"El mayor es {maj}")
+# i = 0
+# 
+# while i <= 10:
+    #   print(i)
+    #   i += 1
 
-med = sum(lista) / len(lista)
-print(f"La media es {med}")
+# creador de contraseña
+
+import random
+import string
+
+print("Bievenido a su generador de contraseñas aleatorio")
+
+letras = input("\n¿Cuantas letras quieres?:")
+
+digitos = input("¿Cuantos digitos quieres?:")
+simbolos = input("¿Cuantos simbolos quieres?:")
